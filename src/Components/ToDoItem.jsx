eed583d053abcc53da2aaf9f4cb528e424a24d63
Shahryar Shagoshtasbi <shahryar9782@gmail.com>
2020-08-18 23:01:12 -0400

    staging for DnD

--- src/Components/ToDoItem.jsx
+++ src/Components/ToDoItem.jsx
@@ -1,27 +1,51 @@
-import React, {useState}from "react";
+import React, {useState, useRef}from "react";
 import "./ToDoItem.css";
 
 export default function ToDoItem(props) {
   const [dragging, setDragging] = useState(false)
+  let draggTask = useRef();
   // https://www.youtube.com/watch?v=Q1PYQPK9TaM
-  const dragStartHandler = (e, id) => {
+  const dragStartHandler = (e, task) => {
+    draggTask.current = task
+    // console.log("Current task is: ", draggTask.current.id)
     setTimeout(() =>{
       setDragging(true);
     }, 0)
   };
 
-  const dragEndhandler = () =>{
+  const dragEndhandler = (e, id) =>{
     setDragging(false);
+  }
+  const dragEnterHandler = (e, id) => {
+    // console.log("Drag Entering ", "\Drag ID: ", id)
+  }
+  const dragEnterOver = (e, nextTaskId) => {
+    // const fromIndex = props.toDo.findIndex((el) => {
+    //   return el.id === id
+    // })
+    // const toIndex = props.toDo.findIndex((el) => {
+    //   return el.id === id
+    // // })
+
+
+    // console.log("Drag over is : ", nextTaskId)
+    // console.log("current Item ID is :" , e)
   }
 
   return (
     <div
       draggable
       onDragStart={(e) => {
-        dragStartHandler(e, props.toDo.id);
+        dragStartHandler(e, props.toDo);
       }}
       onDragEnd={(e) => {
         dragEndhandler(e, props.toDo.id)
+      }}
+      onDragEnter={(e) => {
+        dragEnterHandler(e, props.toDo.id)
+      }}
+      onDragOver={(e) => {
+        dragEnterOver(e, props.toDo.id)
       }}
       className={dragging ? "toDoItem dragging" : "toDoItem"}
 
