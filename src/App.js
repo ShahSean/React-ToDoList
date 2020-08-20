b3dcb6cc2592f1ec2a2e5ee7b15150d5c9f80edd
Shahryar Shagoshtasbi <shahryar9782@gmail.com>
2020-08-20 16:23:58 -0400

    Some Warnings Removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import NavBar from "./Components/NavBar.jsx";
 import ToDoContainer from "./Components/ToDoContainer.jsx";
 import { v4 as uuidv4 } from "uuid";
